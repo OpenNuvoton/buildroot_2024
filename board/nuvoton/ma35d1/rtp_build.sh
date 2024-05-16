a96c7cdfe6ff3e39658297d5096f7887dbfae2a1
Jacky Huang <ychuang3@nuvoton.com>
2024-05-16 15:20:01 +0800

    Add buildroot-2024.02 with support for MA35D1
    Change-Id: Ia9cf21dcb5feda9ff3128d18fbf303f04dfb0f03

---
+++ board/nuvoton/ma35d1/rtp_build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# $1: gcc-arm-none-eabi patch
+# $2: nu-eclipse patch
+# $3: source patch
+# $4: deploy patch
+
+GCC_DIR=$1
+ECLIPSE_DIR=$2
+SOURCE_DIR=$3
+DEPLOY_DIR=$4
+
+export PATH=$PATH:${GCC_DIR}/bin
+export DISPLAY=":99"
+
+ECLIPSE_PAR='-nosplash --launcher.suppressErrors -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data Temp -cleanBuild all -import'
+
+if ! pgrep Xvfb
+then
+	exec Xvfb ":99" &> /dev/null &
+fi
+
+install -d ${DEPLOY_DIR}
+#find ${SOURCE_DIR}/SampleCode -name Release -type d | xargs -i rm {} -r
+#find ${SOURCE_DIR}/SampleCode -name GCC -type d | xargs -i ${ECLIPSE_DIR}/eclipse ${ECLIPSE_PAR} {} &> build.log
+#find ${SOURCE_DIR}/SampleCode -name "*.elf" -type f | xargs -i cp {} ${DEPLOY_DIR}
+
+find ${SOURCE_DIR}/SampleCode -name Release -type d | xargs -i rm {} -r
+find ${SOURCE_DIR}/SampleCode -name GCC -type d | xargs -i echo {} > ProjList.txt
+while read rows
+do
+	${ECLIPSE_DIR}/eclipse ${ECLIPSE_PAR} $rows &>> build.log
+	rm Temp -rf
+done < ProjList.txt
+find ${SOURCE_DIR}/SampleCode -name "*.elf" -type f | xargs -i cp {} ${DEPLOY_DIR}
+
+find ${DEPLOY_DIR} -name "*.elf" -type f | xargs -i echo {} | sed "s/\.elf//" > ProjName.txt
+while read rows
+do
+        ELFNAME=$rows
+        arm-none-eabi-objcopy -O binary $ELFNAME.elf $ELFNAME.bin
+done < ProjName.txt
