4f69b03a464aa84ce6e298b530270e6239d2c26c
Arthur Jiang <twjiang@nuvoton.com>
2024-06-03 20:13:29 -0500

    support tslib input device for resistive touchscreens in lvgl 9.1.0
    It is worth pointing out that this patch for lvgl 9.1.0 still support
    not running lvgl with Wayland.
    
    Change-Id: Ib2f4fe9e754be7fe8ae154347ad982c6b513410a

--- package/lvgl/lvgl.mk
+++ package/lvgl/lvgl.mk
@@ -16,15 +16,37 @@ LVGL_LICENSE_FILES = LICENSE
 
 LVGL_DEPENDENCIES = host-cmake
 
+define LVGL_CONFIGURE_TOUCHSCREEN
+        @if grep -Eq "^BR2_PACKAGE_LVGL_DEFAULT_RESISTIVE_TOUCHSCREEN=y$\" ${BR2_CONFIG}; then \
+		sed -i 's/#define LV_USE_EVDEV.*/#define LV_USE_EVDEV    0/' $(@D)/lv_conf.h; \
+		sed -i 's/#define LV_USE_TSLIB.*/#define LV_USE_TSLIB    1/' $(@D)/lv_conf.h; \
+		sed -i 's/##TSLIB_LIBRARIES##/ts/' $(@D)/CMakeLists.txt; \
+                echo "resistive touchscreen"; \
+        else \
+		sed -i 's/#define LV_USE_EVDEV.*/#define LV_USE_EVDEV    1/' $(@D)/lv_conf.h; \
+		sed -i 's/#define LV_USE_TSLIB.*/#define LV_USE_TSLIB    0/' $(@D)/lv_conf.h; \
+		sed -i 's/##TSLIB_LIBRARIES##//' $(@D)/CMakeLists.txt; \
+                echo "capacitive touchscreen"; \
+        fi
+endef
+LVGL_POST_PATCH_HOOKS += LVGL_CONFIGURE_TOUCHSCREEN
+
 define LVGL_REMOVE_HEADER_FILES
         rm -Rf $(TARGET_DIR)/usr/include/lvgl
+	rm -Rf $(TARGET_DIR)/usr/lib/pkgconfig
 endef
 LVGL_POST_INSTALL_TARGET_HOOKS += LVGL_REMOVE_HEADER_FILES
 
 define LVGL_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 $(@D)/bin/main $(TARGET_DIR)/usr/bin/lvgl_main
-        $(INSTALL) -D -m 0755 $(LVGL_PKGDIR)/S90lvgld \
-                $(TARGET_DIR)/etc/init.d/S90lvgld
+	$(INSTALL) -D -m 0755 $(@D)/bin/main \
+		$(TARGET_DIR)/usr/bin/lvgl_main
+
+	$(INSTALL) -D -m 0755 $(LVGL_PKGDIR)/S90lvgld \
+		$(TARGET_DIR)/etc/init.d/S90lvgld
+
+	@if grep -Eq "^BR2_PACKAGE_LVGL_DEFAULT_RESISTIVE_TOUCHSCREEN=y$\" ${BR2_CONFIG}; then \
+		sed -i 's/##//' $(TARGET_DIR)/etc/init.d/S90lvgld; \
+	fi
 endef
 
 $(eval $(cmake-package))
